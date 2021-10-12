5d987bb141e25da3fd62cf6aa87fff54cfa6b8b0
brendacoolen <92064251+brendacoolen@users.noreply.github.com>
2021-10-12 13:08:48 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+//Ejercicio 1: La Lranjera
+
+let Avanzar: string = "Avanzar";
+let Quitar1: string = "Quitar 1";
+let GirarALaIzquierda: string = "girar a la izquierda";
+console.log(Avanzar);
+console.log(Quitar1);
+console.log(Avanzar);
+console.log(Quitar1);
+console.log(Avanzar);
+console.log(Quitar1);
+console.log(Avanzar);
+console.log(Quitar1);
+console.log(GirarALaIzquierda);
+console.log(Avanzar);
+console.log(Quitar1);
+console.log(Avanzar);
+console.log(Quitar1);
+console.log(Avanzar);
+console.log(Quitar1);
+console.log(Avanzar);
+console.log(Quitar1);
+console.log(Avanzar);
+console.log(Quitar1);
+
+//Ejercicio 2: La Granjera
+
+let Llenar1: string = "Llenar 1";
+console.log(Avanzar);
+console.log(Llenar1);
+console.log(Avanzar);
+console.log(Avanzar);
+console.log(Avanzar);
+console.log(Llenar1);
+console.log(Avanzar);
+console.log(Quitar1);
+console.log(Avanzar);
+console.log(Quitar1);
+
+//Ejercicio 3 : La Granjera
+
+let GirarALaDerecha: string = "Girar a la derecha";
+console.log(GirarALaIzquierda);
+console.log(Avanzar);
+console.log(GirarALaDerecha);
+console.log(Avanzar);
+console.log(Avanzar);
+console.log(GirarALaDerecha);
+console.log(Avanzar);
+console.log(Quitar1);
